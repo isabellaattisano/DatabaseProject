05ba8cbfb91ed7967fff43aee429efa6510adc31
camtaylor2 <118017756+camtaylor2@users.noreply.github.com>
2022-12-06 21:26:18 +0000

    Update StoreData.sql
    add products

--- StoreData.sql
+++ StoreData.sql
@@ -74,6 +74,11 @@ INSERT into product VALUES(seqproduct.nextVal, 'Washed Black Cargo Pocket Jeans'
 INSERT into product VALUES(seqproduct.nextVal, 'Gold Basic Aviator Sunglasses', 'accessories', 12.99, 20);
 INSERT into product VALUES(seqproduct.nextVal, 'Chocolate Satin Corset', 'tops', 25.99, 20);
 
+INSERT into product VALUES(seqproduct.nextVal, 'White Bardot Lac Frill Hem Midi Dress', 'dresses', 70.00, 20);
+INSERT into product VALUES(seqproduct.nextVal, 'Stone Maxi Length Hooded Puffer', 'outerwear', 65.99, 20);
+INSERT into product VALUES(seqproduct.nextVal, 'Pink Ribbed Halterneck Bodycon Dress', 'dresses', 45.99, 20);
+INSERT into product VALUES(seqproduct.nextVal, 'Cream Textured Rib High Waist Flared Pants', 'bottoms', 45.99, 20);
+
 select * from product
 -------------------------------------------------------------------------------------------
 --Display Products 
